71d989879df376f9cbeaf28187852b13fd59fadc
Ayush Jain <85601993+ayushjain079@users.noreply.github.com>
2021-07-11 16:07:36 +0530

    Add files via upload

---
+++ CTP_PROJECT.m
@@ -0,0 +1,275 @@
+clc;
+clear;
+close all;
+
+str='sdflm oimdvsoiiwe kmdsflkjew nnijpojewnmk msdfkjweti kjoidfug ewruhweorhn kvdsfij kjweroijvdsn kjnjo ewhjndsf eowirndfs ejwroiwjefng ewrwet kndsfue nsdpowen mcmnslgj oewir anndshft lsoehnf asken nsdppewrpi asdjnr ssad ksdiruweknr asnbasiurqer  asflkndgp mo jsnbhdsteber kksiaehr ksdhvbqpkamqjas qgqqweh as jasdkfhtepwo vnasbafgtewpkn jaslkfjet qooxzqwenqwrnfc asdlkwet mmajquwe jsdgotplakqwjw nnnsdgjwtr mnnnfdglproer jbqiqweio nvskjnsd prksdjdsbvur jqwpqet  sfnsg nerterhh nknerkjwet njnhdsgy kjhq ajasfatenvbpalamx jwqretnao nsdlgdkjrtg jqwywetnsbashsg kjf kljwghh erop nasnfbe lknqwoit sdgygpweoqwmasmnsdvknvrt lkjast wet khdsfoijt kjerjnsgd lkjhwetiuwt asknvbahasoqrw qqqqqqq ggggggg oapasdf aaaaaaaaaaaaaa ewl mvlkjwportj qwkjreqtnvsdk sqwerujjjasdurpqer nnvkjhw iqoqof mmsdkfsdkpweo sdfnqpqjennf dfvav d prksdjdsbvur jqwpqet  s nfdglproer jbqiqweio nvskjnsd prksdjdsbvur jqwpqet  sfnsg nerterhh nknerkjwet njnhdsgy kjhq ajasfatenvbpalamx jwqretnao nsdlgdkjrtg jqwywetnsbashsg fhtephft lsoehnf a asdjnr ssad ksdirt lkjjaslkfjet qooxzqwenqhh nknerkkjrtg jqwywetnsbashsg kjf kljwghh erop nasnfbe lknqwoit sdgygpweoqwmasmnsdvknvrt lkjast wet khdsfoijt kjerjnsgd lkjhwetiuwt asknvbahasoqrw qqqqqqq ggggggg oapasdf aaaaaaaaaaaaaa ewl mvlkjwportj qwkjreqtnvsdk sqwerujjjasdurpqer nnvkjhw iqoqof mmsdkfsdkpweo sdfnqpqjennf dfvav d prksdjdsbvpwo vnasbafgtewpkn jaslkfjet qooxzqwenqwrnfc asdlkwet mmajquwe jsdgotplakqwjw nnnsdgjwtr mnnnfdglproer jbqi jaslkfjet qooxzqwenqwrnfc asdlkwet mmajquwe jsdgotplakqwjw nnnsdgjwtr mnnnfdglproer jbqiqweio nvskjnsd prksdjdsbvur jqwpqet  sfnsg nerterhh nknerkjwet njnhdsgy kjhq ajasfatenvbpalamx jwqretnao nsdlgdkjrtg jqwywetnsbashsg qqqqq ggggggg oapasdf aaaaaaaaaaaaaa ewl mvlkjwportj qwkjreqtnvsdk sqwerujjjasdurpqer nnvkjhw iqoqof mmsdkfsdkpweo sdfnqpqjennf dfvav d prksdjdsbvur jqwpqet  s nfdglproer jbqiqweio nvskjnsd prksdjdsbvur jqwpqet  sfnsg nerterhh nknerkjwet njnhdsgy kjhq ajasfatenvbpalamx jwqretnao nsdlgdkjrtg jqwywetnsbashsg fhtephft lsoehnf a asdjnr ssad ksdirt lkjjaslkfjet qooxzqwenqhh nknerkkjrtg jqwywetnsbashsg kjf kljwghh erop nasnfbe lknqwoit sdgygpweoqwmasmnsdvknvrt lkjast wet khdsfoijt kjerjnsgd lkjhwetiuwt asknvbahasoqrw qqqqqqq ggggggg oapasdf aaaaaaaaaaaaaa ewl mvlkjwportj qwkjreq nerterhh nknerkjwet njnhdsgy kjhq ajasfatenvbpalamx jwqretnao nsdlgdkjrtg jqwywetnsbashsg kjf et qooxzqwenqwrnfc asdlkwet mmajquwe jsdgotplakqwjw nnnsdgjwtr mnnnfdglproer jbqiqweio nvskjnsd prksdjdsbvur jqwpqet  sfnsg nerterhh nknerkjwet njnhdsgy kjhq ajasfatenvbpalamx jwqretnao nsdlgdkjrtg jqwywetnsbashsg kjf kljwghh erop nasnfbe lknqwoit sdgygpweoqwmasmnsdvknvrt lkjast wet khdsfoijt kjerjnsgd lkjhwetiuwt asknvbahasoqrw qqqqqqq ggggggg oapasdf aaaaaaaaaaaaaa ewl mvlkjwportj qwkjreqtnvsdk sqwerujjjasdurpqer nnvkjhw iqoqof mmsdkfsdkpweo sdfnqpqjennf dfvav d prksdjdsbvur jqwpqet  s nfdglproer jbqiqweio nvskjnsd prksdjdsbvur jqwpqet  sfnsg nerterhh nknerkjwet njnhdsgy kjhq ajasfatenvbpalamx jwqretnao nsdlgdkjrtg jqwywetnsbashsg fhtephft lsoehnf a asdjnr ssad ksdirt lkjjaslkfjet qooxzqwenqhh nknerkkjrtg jaaaaaaaaaaaaaa ewl mvlkjwportj qwkjreqtnvsdk sqwerujjjasdurpqer nnvkjhw iqoqof mmsdkfsdkpweo sdfnqpqjennf dfvav d prksdjdsbvur jqwpqet  s nfdglproer jbqiqweio nvskjnsd prksdjdsbvur jqwpqet  sfnsg nerterhh nknerkjwet njnhdsgy kjhq ajasfatenvbpalamx jwqretnao nsdlgdkjrtg jqwywg kjf kljwghh erop nasnfbe lfhtephft lsoehnf a asdjnr ssdirt lkjjaslkfjet qooxzqwenqhh nknerkkjrtg jqwdsfoijt kjerjnsgd lkjhwetiuwt asknvbahasoqrw qqqqqqq ggggggg oapasdaaaa ewl mvlkjwpokjreq nerterhh nknjnhdsgy kjhq ajasprksdjdsbvur jqwpqet  s nfdglproer jbqiqweio nvskjnsd prksdjdsbvur jqwpqet  sfnsg nerterhh nknerkjwet njnhdsgy kjhq ajasfatenvbpalamx jwqretnao nsdlgdkjrtg jqwywetnsbashsg fhtephft lsoehnf a asdjnr ssad ksdirt lkjjaslkfjet qooxzqwenqhh nknerkkjrtg jqwywetnsbashsg kjf kljwghh erop nasnfbe lknqwoit sdgygpweoqwmasmnsdvknvrt lkjast wet khdsfoijt kjerjnsgd lkjhwetiuwt asknvbahasoqrw qqqqqqq ggggggg oapasdf aaaaaaaaaaaaaa ewl mvlkjwportj qwkjreqtnvsdk sqwerujjjasdurpqer nnvkjhw iqoqof sdfnqqjennf dfvav d prksdjdsbvpwo vnasbafgtekn jaslkfjet qooxzqwenqwrnfc asdlkwet mmajquwe jsdgotplakqwjw nnnsdgjwtr mnnnfdgroer jbqi jaslkfjet qooxzqwenqwrnfc asdlkwet mmajquwe jsdgotpla'
+% str='ayush jain'
+c=1;
+
+%% for (k,1) repetition code (i.e., each digit is repeated/transmitted 'k' times)
+for i=1:2:5
+
+    fprintf('For k = %d',i);
+    encoded_sequence=encode(str);
+    channel_encoded_sequence=channel_encode(encoded_sequence,i);
+    bsc_output_binary_sequence=bsc_channel(channel_encoded_sequence);
+    channel_decoded_sequence=channel_decode(bsc_output_binary_sequence,i);
+    decoded_output=decode(channel_decoded_sequence)
+    
+    %% error after bsc
+    compare1 = bsc_output_binary_sequence-channel_encoded_sequence;
+    count=0;
+    for k=1:length(bsc_output_binary_sequence)
+        if compare1(k)==1 || compare1(k)==-1
+            count = count+1;
+        end
+    end
+    error1 = count; % counting no. of error digits received
+    prob_error_bsc = error1/length(bsc_output_binary_sequence)    
+
+    %% comparing the input sequence with received sequence
+    compare = encoded_sequence-channel_decoded_sequence;
+    count=0;
+    for k=1:length(encoded_sequence)
+        if compare(k)==1 || compare(k)==-1
+            count = count+1;
+        end
+    end
+    error = count; % counting no. of error digits received
+    probability_error = error/length(encoded_sequence)
+    prob_error(c) = probability_error;
+    value_k(c)=i;
+    string_pe=str_cmp(str,decoded_output)
+    str_pe(c)=string_pe;
+    c=c+1;
+end
+stem(value_k, prob_error);
+xlabel('Value of K','Fontweight','bold','FontSize',16,'Color','r');
+ylabel('Probability of Decoding Error','Fontweight','bold','FontSize',16,'Color','r');
+title('Probability of Decoding Error at the Receiver for k=1,3, and 5','Fontweight','bold','FontSize',16,'Color','r');
+axis([0 6 0.05 0.30]);
+
+figure;
+stem(value_k, str_pe);
+xlabel('Value of K','Fontweight','bold','FontSize',16,'Color','r');
+ylabel('Probability error in string','Fontweight','bold','FontSize',16,'Color','r');
+title('Probability error in string at the Receiver for k=1,3, and 5','Fontweight','bold','FontSize',16,'Color','r');
+axis([0 6 0.30 0.90]);
+
+%% Function for Encoder
+function encoded_sequence=encode(str1)
+    bin_enc=[];
+    for i=1:strlength(str1)
+        str=str1(i);
+        if str==' '
+            bin_enc=[bin_enc 0 0 0 0 0];
+        elseif str=='a'
+            bin_enc=[bin_enc 0 0 0 0 1];
+        elseif str=='b'
+            bin_enc=[bin_enc 0 0 0 1 0];
+        elseif str=='c'
+            bin_enc=[bin_enc 0 0 0 1 1];
+        elseif str=='d'
+            bin_enc=[bin_enc 0 0 1 0 0];
+        elseif str=='e'
+            bin_enc=[bin_enc 0 0 1 0 1];
+        elseif str=='f'
+            bin_enc=[bin_enc 0 0 1 1 0];
+        elseif str=='g'
+            bin_enc=[bin_enc 0 0 1 1 1];
+        elseif str=='h'
+            bin_enc=[bin_enc 0 1 0 0 0];
+        elseif str=='i'
+            bin_enc=[bin_enc 0 1 0 0 1];
+        elseif str=='j'
+            bin_enc=[bin_enc 0 1 0 1 0];
+        elseif str=='k'
+            bin_enc=[bin_enc 0 1 0 1 1];
+        elseif str=='l'
+            bin_enc=[bin_enc 0 1 1 0 0];
+        elseif str=='m'
+            bin_enc=[bin_enc 0 1 1 0 1];
+        elseif str=='n'
+            bin_enc=[bin_enc 0 1 1 1 0];
+        elseif str=='o'
+            bin_enc=[bin_enc 0 1 1 1 1];
+        elseif str=='p'
+            bin_enc=[bin_enc 1 0 0 0 0];
+        elseif str=='q'
+            bin_enc=[bin_enc 1 0 0 0 1];
+        elseif str=='r'
+            bin_enc=[bin_enc 1 0 0 1 0];
+        elseif str=='s'
+            bin_enc=[bin_enc 1 0 0 1 1];
+        elseif str=='t'
+            bin_enc=[bin_enc 1 0 1 0 0];
+        elseif str=='u'
+            bin_enc=[bin_enc 1 0 1 0 1];
+        elseif str=='v'
+            bin_enc=[bin_enc 1 0 1 1 0];
+        elseif str=='w'
+            bin_enc=[bin_enc 1 0 1 1 1];
+        elseif str=='x'
+            bin_enc=[bin_enc 1 1 0 0 0];
+        elseif str=='y'
+            bin_enc=[bin_enc 1 1 0 0 1];
+        elseif str=='z'
+            bin_enc=[bin_enc 1 1 0 1 0];
+        end
+    end
+    encoded_sequence=bin_enc;
+end
+
+%% Function for Channel Encoder
+function channel_encoded_sequence=channel_encode(encoded_sequence,k)
+    channel_encoded_sequence=[];
+    for i=1:length(encoded_sequence)
+        E=encoded_sequence(i);
+        if E==0
+            channel_encoded_sequence=[channel_encoded_sequence zeros(1,k-1) E];
+        elseif E==1
+            channel_encoded_sequence=[channel_encoded_sequence ones(1,k-1) E];
+        end
+    end
+    channel_encoded_sequence;
+end
+
+%% Function for BSC Channel
+function bsc_output_binary_sequence=bsc_channel(channel_encoded_sequence)
+    bsc_output_binary_sequence=[];
+    for j=1:length(channel_encoded_sequence)
+        prob = 0 + (1-0) .* rand(1,1);
+        if (prob>=0) && (prob<0.75)
+            bsc_output_binary_sequence(j) = channel_encoded_sequence(j);
+        else
+            if channel_encoded_sequence(j) == 0
+                bsc_output_binary_sequence(j) = 1;
+            else
+                bsc_output_binary_sequence(j) = 0;
+            end
+        end
+    end
+    bsc_output_binary_sequence;
+end
+
+%% Function for Channel Decoder
+function channel_decoded_sequence=channel_decode(bsc_output_binary_sequence,k)
+    % Let no. of ones denoted by 'o'
+    % Let no. of zeros denoted by 'z'
+    count=0;
+    var=1;
+    count1=1;
+    for i=1:length(bsc_output_binary_sequence)
+        dummy(var) = bsc_output_binary_sequence(i);
+        if var==k
+            clear var;
+            var = 1;
+            for j=1:k
+                if dummy(j)==0
+                    count=count+1;
+                end
+            end
+            z=count;
+            o=k-z;
+            if z>o
+                channel_decoded_sequence(count1)=0;
+                count1=count1+1;
+            else
+                channel_decoded_sequence(count1)=1;
+                count1=count1+1;
+            end
+            count=0;
+        else
+            var=var+1;
+        end
+    end    
+    channel_decoded_sequence;
+end
+
+%% Function for Decoder
+function decoded_output=decode(channel_decoded_sequence)
+    decoded_output=[];
+    count=1;
+    for i=1:length(channel_decoded_sequence)
+        D(count)=channel_decoded_sequence(i);
+        if count==5
+            if D==[0 0 0 0 0]
+                decoded_output=[decoded_output ' '];
+            elseif D==[0 0 0 0 1]
+                decoded_output=[decoded_output 'a'];
+            elseif D==[0 0 0 1 0]
+                decoded_output=[decoded_output 'b'];
+            elseif D==[0 0 0 1 1]
+                decoded_output=[decoded_output 'c'];
+            elseif D==[0 0 1 0 0]
+                decoded_output=[decoded_output 'd'];
+            elseif D==[0 0 1 0 1]
+                decoded_output=[decoded_output 'e'];
+            elseif D==[0 0 1 1 0]
+                decoded_output=[decoded_output 'f'];
+            elseif D==[0 0 1 1 1]
+                decoded_output=[decoded_output 'g'];
+            elseif D==[0 1 0 0 0]
+                decoded_output=[decoded_output 'h'];
+            elseif D==[0 1 0 0 1]
+                decoded_output=[decoded_output 'i'];
+            elseif D==[0 1 0 1 0]
+                decoded_output=[decoded_output 'j'];
+            elseif D==[0 1 0 1 1]
+                decoded_output=[decoded_output 'k'];
+            elseif D==[0 1 1 0 0]
+                decoded_output=[decoded_output 'l'];
+            elseif D==[0 1 1 0 1]
+                decoded_output=[decoded_output 'm'];
+            elseif D==[0 1 1 1 0]
+                decoded_output=[decoded_output 'n'];
+            elseif D==[0 1 1 1 1]
+                decoded_output=[decoded_output 'o'];   
+            elseif D==[1 0 0 0 0]
+                decoded_output=[decoded_output 'p'];
+            elseif D==[1 0 0 0 1]
+                decoded_output=[decoded_output 'q'];
+            elseif D==[1 0 0 1 0]
+                decoded_output=[decoded_output 'r'];
+            elseif D==[1 0 0 1 1]
+                decoded_output=[decoded_output 's'];
+            elseif D==[1 0 1 0 0]
+                decoded_output=[decoded_output 't'];
+            elseif D==[1 0 1 0 1]
+                decoded_output=[decoded_output 'u'];
+            elseif D==[1 0 1 1 0]
+                decoded_output=[decoded_output 'v'];
+            elseif D==[1 0 1 1 1]
+                decoded_output=[decoded_output 'w'];
+            elseif D==[1 1 0 0 0]
+                decoded_output=[decoded_output 'x'];
+            elseif D==[1 1 0 0 1]
+                decoded_output=[decoded_output 'y'];
+            elseif D==[1 1 0 1 0]
+                decoded_output=[decoded_output 'z'];  
+            else
+                decoded_output=[decoded_output '#'];
+            end
+            clear count;
+            count=1;
+        else
+            count=count+1;
+        end
+    end
+    decoded_output;
+end
+
+%% String Comparision
+
+function string_pe=str_cmp(str,decoded_output)
+    N = 0;
+    M = 0;
+    for i = 1:length(decoded_output)
+        if str(i) == decoded_output(i)
+            N = N+1;
+        else 
+            M = M+1;
+        end
+    end
+    string_pe = M/length(str);
+end
